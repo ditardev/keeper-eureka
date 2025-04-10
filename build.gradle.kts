7a6026e2bc9c4958189771b300d47a58c1d10de9
Ditar <DitarRogozhnikov@gmail.com>
2025-04-10 18:20:57 +0300

    InitCommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
-	kotlin("plugin.lombok") version "2.1.20"
-	id("io.freefair.lombok") version "8.13"
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.micro"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	toolchain {
