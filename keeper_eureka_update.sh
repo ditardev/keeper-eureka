77f58bb9574d46a77f0df360a84ee283e76cdccb
Ditar <DitarRogozhnikov@gmail.com>
2026-06-23 10:42:26 +0300

    Updated: update.sh

--- keeper_eureka_update.sh
+++ keeper_eureka_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-readonly container_name=keeper_eureka_c
-readonly image_name=keeper_eureka_img
+readonly container_name=keeper-eureka-c
+readonly image_name=keeper-eureka-img
 
 echo $container_name
 echo $image_name
